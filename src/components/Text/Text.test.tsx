cd1988e7ff6ff1f6386805bd1d6e6f7b469437c8
Vittorio Palmisano <vpalmisano@gmail.com>
2022-10-14 09:29:42 +0200

    Revert changes

--- src/components/Text/Text.test.tsx
+++ src/components/Text/Text.test.tsx
@@ -20,8 +20,6 @@ describe('<Text />', () => {
   });
 
   test('should render content twice when there is a dataframe and everyRow is true', async () => {
-    const nameData: string[] = ['Erik', 'Natasha'];
-    const ageData: number[] = [42, 38];
     const props: TextProps = {
       frame: {
         fields: [],
